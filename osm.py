ef1621956305fd5e35a9d90e81dd60c18212599e
ameenkhan07 <ameenkhan07@gmail.com>
2019-07-17 02:04:13 -0400

    added missing logic for tag formatting

--- osm.py
+++ osm.py
@@ -6,6 +6,11 @@ OSM_FILE = "./data/map.osm"
 
 
 class OSM_DATA_MODEL:
+    """The data model for the downloaded osm file.
+    Data manually downloaded from OSM site:
+            https://wiki.openstreetmap.org/wiki/Downloading_data
+    """
+
     def __init__(self):
         self.node_data = []
         self.way_data = []
@@ -14,10 +19,21 @@ class OSM_DATA_MODEL:
         self.fields = ["@lat", "@lon", "@id", "nd"]
         self.node_tags = set()
         self.way_tags = set()
-        self._preprocess_osm_data()
+        self._process_osm_data()
+
+    def _process_osm_data(self) -> dict:
+        """Process the OSM file data to the model
+        """
+
+        headers = []
+
+        with open(OSM_FILE) as fd:
+            doc = xmltodict.parse(fd.read())
+            self._format_node_data(doc["osm"]["node"])
+            self._format_way_data(doc["osm"]["way"])
 
     def _format_node_data(self, node_data):
-        """
+        """Formats the "NODE" element of OSM
         """
         for row in node_data:
             temp = {k.lstrip("@a"): v for k, v in row.items() if k in self.node_fields}
@@ -30,35 +46,24 @@ class OSM_DATA_MODEL:
             self.node_data.append(temp)
 
     def _format_way_data(self, way_data):
-        """
+        """Formats the "WAY" element of OSM
         """
         for row in way_data:
             temp = {k.lstrip("@a"): v for k, v in row.items() if k in self.way_fields}
             temp["timestamp"] = utc_to_epoch(row["@timestamp"])
             if "nd" in temp.keys():
-                if isinstance(temp["nd"], list):
-                    temp["node_id"] = [n["@ref"] for n in temp["nd"]]
-                    del temp["nd"]
+                temp["node_id"] = [n["@ref"] for n in temp["nd"]]
+                del temp["nd"]
             if "tag" in temp.keys():
                 if isinstance(temp["tag"], list):
                     temp["tag"] = {t["@k"]: t["@v"] for t in temp["tag"]}
-                    self.way_tags.update(temp["tag"].keys())
+                if isinstance(temp["tag"], OrderedDict):
+                    temp["tag"] = {temp["tag"]["@k"]: temp["tag"]["@v"]}
+                self.way_tags.update(temp["tag"].keys())
             self.way_data.append(temp)
 
-    def _preprocess_osm_data(self) -> dict:
-        """Preprocess OSM data for MapMatching
-        Data manually downloaded from OSM site:
-            https://wiki.openstreetmap.org/wiki/Downloading_data
         """
 
-        headers = []
-
-        with open(OSM_FILE) as fd:
-            doc = xmltodict.parse(fd.read())
-            node_headers = list(doc["osm"]["node"][0].keys())
-            ways_headers = list(doc["osm"]["way"][0].keys())
-            self._format_node_data(doc["osm"]["node"])
-            self._format_way_data(doc["osm"]["way"])
 
 
 if __name__ == "__main__":
